bc09906ab51814ff3d25020cc96ea7158013da72
Tyler Capstick <zzznsewzzz@gmail.com>
2022-04-11 17:58:27 -0500

    demo night prep

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,6 @@ import foot from "../assets/images/logo4.png";
 export default () => html`
   <header class="firstHeader">
     <!-- <img src=${foot} height="100px" class="pfpFoot" /> -->
-    <div class="jumbotron"></div>
+    <div class="jumbotron">Anime Persona</div>
   </header>
 `;
